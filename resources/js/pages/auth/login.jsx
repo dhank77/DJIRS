4b5fd6c3bdf75732c501f88689a30544c1eba419
Hamdani Ilham <d41113512@gmail.com>
2024-06-21 05:59:44 +0800

    persistent layout

--- resources/js/pages/auth/login.jsx
+++ resources/js/pages/auth/login.jsx
@@ -2,15 +2,11 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link, router } from "@inertiajs/react";
-import { useEffect, useState } from "react";
-import { Toaster } from "@/components/ui/sonner"
-import { toast } from "sonner"
-
-
+import { useState } from "react";
+import Guest from "../layouts/guest";
 
 export default function Login(props) {
-
-  const { errors } = props
+  const { errors } = props;
   const [values, setValues] = useState({
     username: "",
     password: "",
@@ -26,77 +22,67 @@ export default function Login(props) {
   }
 
   return (
-    <>
-      <Toaster />
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-        
-        <div className="flex items-center justify-center py-12">
-          <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Login</h1>
-              <p className="text-balance text-muted-foreground">
-                Enter your username below to login to your account
-              </p>
-            </div>
-            <div className="grid gap-4">
-              <form onSubmit={onSubmit}>
-                <div className="grid gap-2 mb-2">
-                  <Label htmlFor="username">Username</Label>
-                  <Input
-                    id="username"
-                    type="text"
-                    name="username"
-                    placeholder="m@example.com"
-                    onChange={changeValue}
-                    value={values.username}
-                    required
-                  />
-                  {errors?.username && ( <div className="text-red-500"> {errors.username} </div> )}
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <Link
-                      href="/forgot-password"
-                      className="ml-auto inline-block text-sm underline"
-                    >
-                      Forgot your password?
-                    </Link>
-                  </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    name="password"
-                    required
-                    placeholder="Password"
-                    onChange={changeValue}
-                    value={values.password}
-                  />
-
-                  {errors?.password && ( <div className="text-red-500"> {errors.password} </div> )}
-                </div>
-                <Button type="submit" className="w-full mt-8">
-                  Login
-                </Button>
-              </form>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link href="/" className="underline">
-                Back to Home
+    <div className="mx-auto grid w-[400px] gap-6">
+      <div className="grid gap-2 text-center">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <p className="text-balance text-muted-foreground">
+          Enter your username below to login to your account
+        </p>
+      </div>
+      <div className="grid gap-4">
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-2 mb-2">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={changeValue}
+              value={values.username}
+              required
+            />
+            {errors?.username && (
+              <div className="text-red-500"> {errors.username} </div>
+            )}
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                href="/forgot-password"
+                className="ml-auto inline-block text-sm underline"
+              >
+                Forgot your password?
               </Link>
             </div>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              required
+              placeholder="Password"
+              onChange={changeValue}
+              value={values.password}
+            />
+
+            {errors?.password && (
+              <div className="text-red-500"> {errors.password} </div>
+            )}
           </div>
-        </div>
-        <div className="hidden bg-muted lg:block">
-          <img
-            src="/media/django.jpg"
-            alt="Image"
-            width="1920"
-            className="h-screen w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </div>
+          <Button type="submit" className="w-full mt-8">
+            Login
+          </Button>
+        </form>
       </div>
-    </>
+      <div className="mt-4 text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link href="/register" className="underline">
+          Register
+        </Link>
+      </div>
+    </div>
   );
 }
+
+Login.layout = (page) => <Guest children={page} />
---
+++ resources/js/pages/layouts/guest.jsx
@@ -0,0 +1,17 @@
+export default function Guest({ children }) {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="flex items-center justify-center py-12">
+        {children}
+    </div>
+    <div className="hidden bg-muted lg:block">
+        <img
+        src="/media/django.jpg"
+        alt="Image"
+        width="1920"
+        className="h-screen w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+    </div>
+    </div>
+  )
+}
